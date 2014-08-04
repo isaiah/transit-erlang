28a7c400d8602f15a3d17ccecd827801f70d5331
isaiah <issaria@gmail.com>
2014-08-04 17:36:36 +0200

    include jsx as deps, implement varias decode funs

--- src/transit_reader.erl
+++ src/transit_reader.erl
@@ -11,7 +11,7 @@
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/0]).
+-export([start_link/0, read/1]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -27,41 +27,57 @@
 start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
+read(Name) ->
+  gen_server:call(?MODULE, {read, jsx:decode(Name)}).
+
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
 init([]) ->
   transit_rolling_cache:start_link(),
-  State = #state{},
-  {ok, State}.
+  AsMapKey = #state{},
+  {ok, AsMapKey}.
 
-handle_call({read, Name}, _From, State) ->
-  {Val, S1} = decode_string(Name, State),
-  {reply, Val, S1};
-handle_call(_Request, _From, State) ->
-  {reply, ok, State}.
+handle_call({read, Name}, _From, AsMapKey) ->
+  Val = decode(Name, false),
+  {reply, Val, AsMapKey};
+handle_call(_Request, _From, AsMapKey) ->
+  {reply, ok, AsMapKey}.
 
-handle_cast(_Msg, State) ->
-  {noreply, State}.
+handle_cast(_Msg, AsMapKey) ->
+  {noreply, AsMapKey}.
 
-handle_info(_Info, State) ->
-  {noreply, State}.
+handle_info(_Info, AsMapKey) ->
+  {noreply, AsMapKey}.
 
-terminate(_Reason, _State) ->
+terminate(_Reason, _AsMapKey) ->
   ok.
 
-code_change(_OldVsn, State, _Extra) ->
-  {ok, State}.
+code_change(_OldVsn, AsMapKey, _Extra) ->
+  {ok, AsMapKey}.
 
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
+decode(Name, AsMapKey) when is_bitstring(Name) ->
+  decode_string(Name, AsMapKey);
+decode(Name, AsMapKey) when is_list(Name) ->
+  case Name of
+    [?MAP_AS_ARR|Tail] ->
+      decode_array_hash(Tail, AsMapKey);
+    [[?ESC, "#"|Tag], Rep] ->
+      decode_tag(Tag, Rep, AsMapKey);
+    _ ->
+      decode_array(Name, AsMapKey)
+  end;
+decode(Name, AsMapKey) ->
+  decode_hash(Name, AsMapKey).
 
-decode_string(String, State=#state{env=Env}) ->
-  parse_string(transit_rolling_cache:decode(String, transit_marshaler:as_map_key(Env)), State).
+decode_string(String, AsMapKey=#state{env=Env}) ->
+  parse_string(transit_rolling_cache:decode(String, transit_marshaler:as_map_key(Env)), AsMapKey).
 
-parse_string(String, State=#state{}) ->
+parse_string(String, AsMapKey=#state{}) ->
   Val = case String of
           [?ESC,Tag|Rep] ->
             case transit_read_handlers:handler(Tag) of
@@ -79,7 +95,31 @@ parse_string(String, State=#state{}) ->
           _ ->
             String
         end,
-  {Val, State}.
+  {Val, AsMapKey}.
+
+decode_array_hash(Name, AsMapKey) ->
+  [{decode(Key, true), decode(Val, AsMapKey)} || {Key, Val} <- Name].
+
+decode_array(Name, AsMapKey) ->
+  [decode(El, AsMapKey) || El <- Name].
+
+decode_tag(Tag, Rep, AsMapKey) ->
+  F = transit_read_handler:handler(Tag),
+  {F(Rep), AsMapKey}.
+
+decode_hash(Name, AsMapKey) when length(Name) =:= 1 ->
+  [{decode(Key, AsMapKey), decode(Val, AsMapKey)} || {Key, Val} <- Name];
+decode_hash(Name, AsMapKey) ->
+  case Name of
+    {[?ESC, "#"|Tag], Val} ->
+      decode_tag(Tag, Val, AsMapKey);
+    {Key, Val} ->
+      {Rep, _} = decode(Val, AsMapKey),
+      {Key, Rep};
+    _ ->
+      erlang:throw("unkown hash format")
+  end.
+
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
@@ -97,8 +137,8 @@ unmarshal_test_() ->
    fun stop/1,
    []}.
 
-unmarshal_string(ok) ->
+unmarshal_quoted(ok) ->
   Env = transit_marshaler:new_env(),
-  Tests = [{1, "[\"~#'\", 1]"}],
-  [fun() -> {Val, _} = decode_string(Str, #state{env=Env}) end || {Val, Str} <- Tests].
+  Tests = [{1, <<"[\"~#'\", 1]">>}],
+  [fun() -> {Val, _} = decode(jsx:decode(Str), #state{env=Env}) end || {Val, Str} <- Tests].
 -endif.
