c1709a12e80c8d2f470b605c49ffa2009ea8587b
isaiah <issaria@gmail.com>
2014-08-16 09:19:07 +0200

    move every exemplar test into a single testcase

--- test/exemplar_SUITE.erl
+++ test/exemplar_SUITE.erl
@@ -1,7 +1,35 @@
 -module(exemplar_SUITE).
 -include_lib("common_test/include/ct.hrl").
 -export([all/0]).
--export([exemplar_tests/1, init_per_suite/1, end_per_suite/1]).
+
+%%% exemplar tests
+-export([nil_exemplar/1,
+         false_exemplar/1,
+         true_exemplar/1,
+         zero_exemplar/1,
+         one_exemplar/1,
+         one_string_exemplar/1,
+         one_keyword_exemplar/1,
+         one_symbol_exemplar/1,
+         one_date_exemplar/1,
+         vector_simple_exemplar/1,
+         vector_empty_exemplar/1,
+         vector_mixed_exemplar/1,
+         vector_nested_exemplar/1,
+         small_strings_exemplar/1,
+         strings_tilde_exemplar/1,
+         strings_hash_exemplar/1,
+         strings_hat_exemplar/1,
+         ints_exemplar/1,
+         small_ints_exemplar/1,
+         doubles_small_exemplar/1,
+         doubles_interesting_exemplar/1,
+         one_uuid_exemplar/1,
+         uuids_exemplar/1,
+         one_uri_exemplar/1,
+         uris_exemplar/1
+        ]).
+
 
 -define(datadir, "../../transit-format/examples/0.8/simple/").
 
@@ -26,47 +54,89 @@
 -define(DATES, lists:map(fun(X) -> transit_types:datetime(X) end,
                          [{-6106,01760,0}, {0,0,0}, {946,72800,0}, {1396,90903,7}])).
 
-all() -> [exemplar_tests].
+all() -> [nil_exemplar,
+          false_exemplar,
+          true_exemplar,
+          zero_exemplar,
+          one_exemplar,
+          one_string_exemplar,
+          one_keyword_exemplar,
+          one_symbol_exemplar,
+          one_date_exemplar,
+          vector_simple_exemplar,
+          vector_empty_exemplar,
+          vector_mixed_exemplar,
+          vector_nested_exemplar,
+          small_strings_exemplar,
+          strings_tilde_exemplar,
+          strings_hash_exemplar,
+          strings_hat_exemplar,
+          ints_exemplar,
+          small_ints_exemplar,
+          doubles_small_exemplar,
+          doubles_interesting_exemplar,
+          one_uuid_exemplar,
+          uuids_exemplar,
+          one_uri_exemplar,
+          uris_exemplar
+         ].
 
-init_per_suite(Config) ->
-  Config.
+nil_exemplar(Conf) ->
+  exemplar("nil", undefined, Conf).
+false_exemplar(Conf) ->
+  exemplar("false", false, Conf).
+true_exemplar(Conf) ->
+  exemplar("true", true, Conf).
+zero_exemplar(Conf) ->
+  exemplar("zero", 0, Conf).
+one_exemplar(Conf) ->
+  exemplar("one", 1, Conf).
+one_string_exemplar(Conf) ->
+  exemplar("one_string", <<"hello">>, Conf).
+one_keyword_exemplar(Conf) ->
+  exemplar("one_keyword", hello, Conf).
+one_symbol_exemplar(Conf) ->
+  exemplar("one_symbol", transit_types:symbol("hello"), Conf).
+one_date_exemplar(Conf) ->
+  exemplar("one_date", transit_types:datetime({946,72800,0}), Conf).
+vector_simple_exemplar(Conf) ->
+  exemplar("vector_simple", ?ArraySimple, Conf).
+vector_empty_exemplar(Conf) ->
+  exemplar("vector_empty", [], Conf).
+vector_mixed_exemplar(Conf) ->
+  exemplar("vector_mixed", ?ArrayMixed, Conf).
+vector_nested_exemplar(Conf) ->
+  exemplar("vector_nested", ?ArrayNested, Conf).
+small_strings_exemplar(Conf) ->
+  exemplar("small_strings", ?SmallStrings, Conf).
+strings_tilde_exemplar(Conf) ->
+  exemplar("strings_tilde", lists:map(fun(X) -> <<"~", X/binary>> end, ?SmallStrings), Conf).
+strings_hash_exemplar(Conf) ->
+  exemplar("strings_hash", lists:map(fun(X) -> <<"#", X/binary>> end, ?SmallStrings), Conf).
+strings_hat_exemplar(Conf) ->
+  exemplar("strings_hat", lists:map(fun(X) -> <<"^", X/binary>> end, ?SmallStrings), Conf).
+ints_exemplar(Conf) ->
+  exemplar("ints", lists:seq(0,127), Conf).
+small_ints_exemplar(Conf) ->
+  exemplar("small_ints", lists:seq(-5, 5), Conf).
+  %exemplar("ints_interesting", ?INTERESTING_INTS, Conf),
+  %exemplar("ints_interesting_neg", lists:map(fun(X) -> -X end, ?INTERESTING_INTS), Conf),
+doubles_small_exemplar(Conf) ->
+  exemplar("doubles_small", lists:map(fun(X) -> float(X) end, lists:seq(-5, 5)), Conf).
+doubles_interesting_exemplar(Conf) ->
+  exemplar("doubles_interesting", [-3.14159, 3.14159, 4.0E11, 2.998E8, 6.626E-34], Conf).
+one_uuid_exemplar(Conf) ->
+  exemplar("one_uuid", lists:nth(1, ?UUIDS), Conf).
+uuids_exemplar(Conf) ->
+  exemplar("uuids", ?UUIDS, Conf).
+one_uri_exemplar(Conf) ->
+  exemplar("one_uri", lists:nth(1, ?URIS), Conf).
+uris_exemplar(Conf) ->
+  exemplar("uris", ?URIS, Conf).
+  %exemplar("dates_interesting", ?DATES, Conf),
 
-end_per_suite(Config) ->
-  Config.
-
-exemplar_tests(Config) ->
+exemplar(Name, Val, Config) ->
   Dir = ?config(data_dir, Config),
-  exemplar("nil", undefined, Dir),
-  exemplar("false", false, Dir),
-  exemplar("true", true, Dir),
-  exemplar("zero", 0, Dir),
-  exemplar("one", 1, Dir),
-  exemplar("one_string", <<"hello">>, Dir),
-  exemplar("one_keyword", hello, Dir),
-  exemplar("one_symbol", transit_types:symbol("hello"), Dir),
-  exemplar("one_date", transit_types:datetime({946,72800,0}), Dir),
-  exemplar("vector_simple", ?ArraySimple, Dir),
-  exemplar("vector_empty", [], Dir),
-  exemplar("vector_mixed", ?ArrayMixed, Dir),
-  exemplar("vector_nested", ?ArrayNested, Dir),
-  exemplar("small_strings", ?SmallStrings, Dir),
-  exemplar("strings_tilde", lists:map(fun(X) -> <<"~", X/binary>> end, ?SmallStrings), Dir),
-  exemplar("strings_hash", lists:map(fun(X) -> <<"#", X/binary>> end, ?SmallStrings), Dir),
-  exemplar("strings_hat", lists:map(fun(X) -> <<"^", X/binary>> end, ?SmallStrings), Dir),
-  exemplar("ints", lists:seq(0,127), Dir),
-  exemplar("small_ints", lists:seq(-5, 5), Dir),
-  %exemplar("ints_interesting", ?INTERESTING_INTS, Dir),
-  %exemplar("ints_interesting_neg", lists:map(fun(X) -> -X end, ?INTERESTING_INTS), Dir),
-  exemplar("doubles_small", lists:map(fun(X) -> float(X) end, lists:seq(-5, 5)), Dir),
-  exemplar("doubles_interesting", [-3.14159, 3.14159, 4.0E11, 2.998E8, 6.626E-34], Dir),
-  exemplar("one_uuid", lists:nth(1, ?UUIDS), Dir),
-  exemplar("uuids", ?UUIDS, Dir),
-  exemplar("one_uri", lists:nth(1, ?URIS), Dir),
-  exemplar("uris", ?URIS, Dir),
-  %exemplar("dates_interesting", ?DATES, Dir),
-  ok.
-
-exemplar(Name, Val, Dir) ->
   lists:map(fun(Ext) ->
                 File = filename:join(Dir, Name ++ "." ++ atom_to_list(Ext)),
                 {ok, Data} = file:read_file(File),
