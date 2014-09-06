d84159639468a42654ab6f268032f125489e1095
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2014-09-07 00:36:09 +0200

    Better atom() generation, tests for timestamps in verbose formats.

--- eqc_test/transit_eqc.erl
+++ eqc_test/transit_eqc.erl
@@ -5,10 +5,20 @@
 
 %% Generate a valid atom in Erlang land (we can't handle utf8 yet here before R18
 
-simple_atom() -> elements([a,b,c,d,e,f]).
-advanced_atom() -> 
-  ?LET(Vec, vector(2, choose(0, 127)),
-    list_to_atom(Vec)).
+simple_atom() ->
+	oneof([
+	  elements([a,b,c,d,e,f]),
+	  elements([phineas, ferb, candace, doofenschmirtz, isabella, perry, major_monogram])
+	]).
+
+random_atoms() ->
+	elements(
+	  ['d 6y_>\b\007',':\007=&1CUH','o!,:T(\\U','bl\r\037n\0222w',
+	   '\031\b\vFt8/D','P-\005kaUVK','\002-F<\vS\0320',
+	   '\026,g\027!WOP','x\b\bPIi9F','\007{\002\037yKcB']).
+	
+advanced_atom() ->
+  random_atoms().
 
 atom() ->
     ?SHRINK(advanced_atom(),
@@ -70,10 +80,10 @@ transit(0) ->
 transit(N) ->
     frequency([
         {1, transit(0)},
-        {N, ?LAZY(array(transit(N div 2)))},
-        {N, ?LAZY(set(transit(N div 2)))},
+        {N, ?LAZY(array(transit(N div 4)))},
+        {N, ?LAZY(set(transit(N div 4)))},
         {N, ?LAZY(transit_map(transit(N div 6), transit(N div 6)))},
-        {N, ?LAZY(list(transit(N div 2)))}
+        {N, ?LAZY(list(transit(N div 4)))}
     ]).
 
 term() ->
@@ -101,6 +111,16 @@ prop_uuid() -> ?FORALL(UUID, transit_uuid(), iso(json, UUID)).
 prop_string() -> ?FORALL(S, eqc_lib:utf8_string(), iso(json, S)).
 prop_keyword() -> ?FORALL(KW, keyword(), iso(json, KW)).
 prop_symbol() -> ?FORALL(S, symbol(), iso(json, S)).
+
+prop_timestamp_json() ->
+  ?FORALL(TS, transit_time(),
+    iso(json, TS)).
+prop_timestamp_json_verbose() ->
+  ?FORALL(TS, transit_time(),
+    iso(json_verbose, TS)).
+prop_timestamp_msgpack() ->
+  ?FORALL(TS, transit_time(),
+    iso(msgpack, TS)).
 
 ms({Mega, Secs, Micro}) -> {Mega, Secs, (Micro div 1000) * 1000}.
 
