3d3b04ee6aadcc6d5e109b47b6045f44841bed64
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2014-09-09 14:45:05 +0200

    Add a simplistic benchmark.
    In order to make sure we don't do something bad, we need to establish a base case
    of benchmark runs.

---
+++ bench/bench.erl
@@ -0,0 +1,38 @@
+-module(bench).
+
+-export([run/0, run/1]).
+
+-define(ROUNDS, 1000).
+
+run() ->
+	run("../../transit-format/examples/0.8/example.json").
+	
+run(File) ->
+	{ok, RData} = file:read_file(File),
+	WData = transit:read(RData),
+	io:format("Read~n"),
+	{ReadTiming, ok} = timer:tc(fun() -> run_read(?ROUNDS, RData) end),
+	io:format("Write~n"),
+	{WriteTiming, ok} = timer:tc(fun() -> run_write(?ROUNDS, WData) end),
+	io:format("ISO~n"),
+	{IsoTiming, ok} = timer:tc(fun() -> run_iso(?ROUNDS, RData) end),
+	#{
+	  read => ReadTiming / ?ROUNDS,
+	  write => WriteTiming / ?ROUNDS,
+	  iso => IsoTiming / ?ROUNDS
+	}.
+	
+
+run_read(0, _Data) -> ok;
+run_read(N, Data) ->
+	transit:read(Data),
+	run_read(N-1, Data).
+	
+run_write(N, Data) ->
+	transit:write(Data),
+	run_write(N-1, Data).
+
+run_iso(N, Data) ->
+	Read = transit:read(Data),
+	transit:write(Read),
+	run_iso(N-1, Data).
