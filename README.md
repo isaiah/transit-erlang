2733e64f0ed15cacbed1f0534e0eef86a1ce99a3
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2014-09-09 15:10:41 +0200

    Report timings for all formats.

--- README.md
+++ README.md
@@ -37,14 +37,21 @@ These benchmarks are run on a Lenovo Thinkpad W540 with a 16 Gigabyte RAM config
 
 	Intel(R) Core(TM) i7-4900MQ CPU @ 2.80GHz
 
-Timings run 1000 rounds of encoding of the file `transit-format/examples/0.8/example.json` and then we divide down to get the
+Timings run 300 rounds of encoding of the file `transit-format/examples/0.8/example.json` and then we divide down to get the
 encoder time for each round. This then forms the base benchmark.
 
-| Commit | Test | Timing μs |
-| ------ | ---- | --------- |
-| 3d3b04ee6aad | Read | 33140 |
-| 3d3b04ee6aad | Write | 10115 |
-| 3d3b04ee6aad | ISO | 21313 |
+| Commit | Test | Format | Timing ms |
+| ------ | ---- | ------ | --------- |
+| 3d3b04ee6aad | JSON | Read | 9.976 |
+| 3d3b04ee6aad | JSON | Write | 20.810 |
+| 3d3b04ee6aad | JSON | ISO | 31.987 |
+| 3d3b04ee6aad | MsgPack | Read | 4.901 |
+| 3d3b04ee6aad | MsgPack | Write | 12.072 |
+| 3d3b04ee6aad | MsgPack | ISO | 15.911 |
+| 3d3b04ee6aad | JSON_Verbose | Read | 9.724 |
+| 3d3b04ee6aad | JSON_Verbose | Write | 25.638 |
+| 3d3b04ee6aad | JSON_Verbose | ISO | 34.236 |
+
 
 Current limitations
 --------------------
--- bench/bench.erl
+++ bench/bench.erl
@@ -1,40 +1,40 @@
 -module(bench).
 
--export([run/0, run/1]).
+-export([run/1, run/2]).
 
--define(ROUNDS, 1000).
+-define(ROUNDS, 300).
 
-run() ->
-	run("../../transit-format/examples/0.8/example.json").
+run(Format) ->
+	run(Format, "../../transit-format/examples/0.8/example.json").
 	
-run(File) ->
-	{ok, RData} = file:read_file(File),
-	WData = transit:read(RData),
+run(Format, File) ->
+	{ok, Data} = file:read_file(File),
+	WData = transit:read(Data),
+	RData = transit:write(WData, [{format, Format}]),
 	io:format("Read~n"),
-	{ReadTiming, ok} = timer:tc(fun() -> run_read(?ROUNDS, RData) end),
+	{ReadTiming, ok} = timer:tc(fun() -> run_read(?ROUNDS, RData, [{format, Format}]) end),
 	io:format("Write~n"),
-	{WriteTiming, ok} = timer:tc(fun() -> run_write(?ROUNDS, WData) end),
+	{WriteTiming, ok} = timer:tc(fun() -> run_write(?ROUNDS, WData, [{format, Format}]) end),
 	io:format("ISO~n"),
-	{IsoTiming, ok} = timer:tc(fun() -> run_iso(?ROUNDS, RData) end),
+	{IsoTiming, ok} = timer:tc(fun() -> run_iso(?ROUNDS, RData, [{format, Format}]) end),
 	#{
 	  read => ReadTiming / ?ROUNDS,
 	  write => WriteTiming / ?ROUNDS,
 	  iso => IsoTiming / ?ROUNDS
 	}.
 	
-
-run_read(0, _Data) -> ok;
-run_read(N, Data) ->
-	transit:read(Data),
-	run_read(N-1, Data).
+run_read(0, _Data, _Opts) -> ok;
+run_read(N, Data, Opts) ->
+	transit:read(Data, Opts),
+	run_read(N-1, Data, Opts).
 	
-run_write(0, _Data) -> ok;
-run_write(N, Data) ->
-	transit:write(Data),
-	run_write(N-1, Data).
+run_write(0, _Data, _Opts) -> ok;
+run_write(N, Data, Opts) ->
+	transit:write(Data, Opts),
+	run_write(N-1, Data, Opts).
 
-run_iso(0, _Data) -> ok;
-run_iso(N, Data) ->
-	Read = transit:read(Data),
-	transit:write(Read),
-	run_iso(N-1, Data).
+run_iso(0, _Data, _Opts) -> ok;
+run_iso(N, Data, Opts) ->
+	Read = transit:read(Data, Opts),
+	transit:write(Read, Opts),
+	run_iso(N-1, Data, Opts).
