d951179351bad505594da166d78ccda13b395d67
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2014-09-17 23:01:05 +0200

    Use maps as configuration by default.

--- src/transit.erl
+++ src/transit.erl
@@ -1,12 +1,14 @@
 -module(transit).
 -export([read/1, read/2, write/1, write/2]).
 
-read(Rep) -> read(Rep, [{format, json}]).
+read(Rep) ->
+	read(Rep, #{ format => json }).
 
 read(Rep, Opts) ->
   transit_reader:read(Rep, Opts).
 
-write(Rep) -> write(Rep, [{format, json}]).
-
+write(Rep) ->
+	write(Rep, #{ format => json }).
+	
 write(Rep, Opts) ->
   transit_writer:write(Rep, Opts).
